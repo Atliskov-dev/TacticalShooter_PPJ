245f2f004157627a9a821acc08d5a51ee3f5ec4f
Atliskov-dev <alimdogan.01@outlook.com>
2024-04-08 19:24:18 +0200

    HardMoments

--- TacticalShooter/Assets/Scripts/PlayerScripts/PistolCombat.cs
+++ TacticalShooter/Assets/Scripts/PlayerScripts/PistolCombat.cs
@@ -1,31 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PistolCombat : MonoBehaviour
 {
     public Animator anim;
     public Animator aimAnim;
 
-    //public CameraShake camShake;
-    public HeadBob bobbing;
 
+    public HeadBob bobbing;
     public Player_Movement movement;
 
     //CamShake
     public Transform mainCamera;
 
+    //PostProcessing
+    public Volume volume;
+    float shootingTime = 0.2f;
+    public bool isShooting;
+    public float blurFloat;
+    public int blurInt;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)&& movement.running == false) 
         {
+            isShooting = true;
             anim.Play("Base Layer.metarig_Fire", 0, 0);
-            //camShake.MakeTrauma(0.1f);
-            
             mainCamera.Rotate(Vector3.forward, 1500 * Time.deltaTime);
             mainCamera.Rotate(Vector3.right, 200* Random.Range(1f,-1f) * Time.deltaTime);
         }
+        if (isShooting) 
+        {
+            shootingTime -= Time.deltaTime;
+            if (shootingTime <= 0) 
+            {
+                shootingTime = 0.2f;
+                isShooting = false;
+            }
+
+            blurFloat = 12f;
+        }
+        else 
+        {
+            blurFloat = Mathf.Lerp(blurFloat, 1, Time.deltaTime * 1f);
+
+        }
+
+        blurInt = (int)blurFloat;
+        volume.profile.TryGet(out DepthOfField depthOfField);
+        depthOfField.focalLength.value = blurInt;
+
+
+
+
+
+
         if (Input.GetKeyDown(KeyCode.R) && movement.running == false) 
         {
             anim.Play("Base Layer.metarig_Reload", 0, 0);
